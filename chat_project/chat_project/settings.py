384ab999f0963ef5d9d0ee8dd7a135ddc557c50d
himanshu-sharmav <himan9506492198@gmail.com>
2025-02-01 20:15:49 +0530

    / in static

--- chat_project/chat_project/settings.py
+++ chat_project/chat_project/settings.py
@@ -133,7 +133,7 @@ CHANNEL_LAYERS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'chat', 'static'),
 ]
